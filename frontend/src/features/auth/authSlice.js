4ff95f8d0006fc0cf51d14fbfacc4bf34a699b11
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-01 21:43:37 -0600

    Update authService

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from '../authService'
 
 // Get user from localStorage
 const user = JSON.parse(localStorage.getItem('user'))
@@ -11,8 +12,18 @@ const initialState = {
   message: '',
 }
 
+// Register user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+  try {
+    return await authService.register(user)
+  } catch (error) {
+    const message = (error.response && error.response.data && error.response.data.message) || error. message || error.toString()
+    return thunkAPI.rejectWithValue(message)
+  }
+})
+
 export const authSlice = createSlice({
-  name: 'auth',
+  name: 'auth', 
   initialState,
   reducers: {
     reset: (state) => {
@@ -22,7 +33,7 @@ export const authSlice = createSlice({
       state.message = ''
     }
   },
-  extraReducers: () => {}
+  extraReducers: () => {},
 })
 export const {reset } = authSlice.actions
 export default authSlice.reducer
