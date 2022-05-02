d3c198a86ac036f2206017fefde4cc3c26485bff
Jonghan Park <jonghan.park@edu.sait.ca>
2022-05-01 20:01:08 -0600

    Update login and register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,53 @@
+import { useState, useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const {email, password} = formData
+
+  const onChange = (e) =>{
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+   
+  const onSubmit = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <section className='heading'>
+      <h1>
+        <FaSignInAlt />Login
+      </h1>
+      <p>Login and start setting goals</p>
+    </section>
+    <section className="form">
+      <div className="form-group">
+      <form>
+        <input type="email" className="form-control" id="email"
+        name='email' value={email} placeholder="Enter your email"
+        onChange={onChange} />
+      </form>
+      </div>
+      <div className="form-group">
+      <form>
+        <input type="password" className="form-control" id="password"
+        name='password' value={password} placeholder="Enter password"
+        onChange={onChange} />
+      </form>
+      </div>
+      <div className="form-group">
+        <button type='submit' className='btn btn-block'>Submit</button>
+      </div>
+    </section>
+    </>
   )
 }
 
